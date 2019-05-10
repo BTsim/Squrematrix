21b04a5f953cfa710e2adcb65c5211a95962ddb7
Bahdan Tsimashevich <timoh.manutd@gmail.com>
2019-05-10 11:15:15 +0300

    Completed homework_advanced_1 (squrematrix)

---
+++ Squrematrix/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squrematrix
+{
+    class Rectangle : IFigure
+    {
+        public double sideA { get; set; }
+        public double sideB { get; set; }
+        public double area { get; set; }
+        public Rectangle(double A, double B)
+        {
+            sideA = A;
+            sideB = B;
+            FigureArea();
+        }
+        public void FigureArea()
+        {
+            area = sideA * sideB;
+        }
+
+    }
+}
---
+++ Squrematrix/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squrematrix
+{
+    class Triangle : IFigure
+    {
+        public double sideA { get; set; }
+        public double height { get; set; }
+        public double area { get; set; }
+        public Triangle(double a, double h)
+        {
+            sideA = a;
+            height = h;
+            FigureArea();
+
+        }
+        public void FigureArea()
+        {
+            area = sideA * height / 2;
+        }
+    }
+}
